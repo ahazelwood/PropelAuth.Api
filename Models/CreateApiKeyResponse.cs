61fdd0f78eaf840394f4d8a6a9554263f7aa968d
ahazelwood <ahazelwood@ciradar.com>
2025-02-25 15:44:34 -0500

    Files created from code reorganization.

---
+++ Models/CreateApiKeyResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PropelAuth.Api.Models
+{
+    public class CreateApiKeyResponse
+    {
+        public required string ApiKeyId { get; set; }
+        public required string ApiKeyToken { get; set; }
+    }
+}
---
+++ Models/FindApiKeyResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PropelAuth.Api.Models
+{
+    public class FindApiKeyResponse {
+        [JsonPropertyName("api_keys")]
+        public List<PropelAuthApiKey> ApiKeys { get; set; } = [];
+
+        [JsonPropertyName("total_api_keys")]
+        public int TotalApiKeys { get; set; }
+
+        [JsonPropertyName("current_page")]
+        public int CurrentPage { get; set; }
+
+        [JsonPropertyName("page_size")]
+        public int PageSize { get; set; }
+
+        [JsonPropertyName("has_more_results")]
+        public bool HasMoreResults { get; set; }
+    }
+}
